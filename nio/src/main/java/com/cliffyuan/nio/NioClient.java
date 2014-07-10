2ca20688c4291776a62110a57858677b922f1973
cliffyuan <xiaoniudu2012@gmail.com>
2014-07-10 23:10:43 +0800

    nio configureBlocking

--- nio/src/main/java/com/cliffyuan/nio/NioClient.java
+++ nio/src/main/java/com/cliffyuan/nio/NioClient.java
@@ -8,18 +8,30 @@ import java.nio.*;
 import java.nio.channels.SocketChannel;
 
 /**
+ * 建立远程连接
+ * 注意点：
+ * （1）必须设置flip()，否则数据无法写入
+ *
  * Created by yuanyuanming on 14-7-10.
  */
 public class NioClient {
     public static void main(String[] args)throws IOException,InterruptedException{
         SocketChannel socketChannel=SocketChannel.open();
         socketChannel.connect(new InetSocketAddress("127.0.0.1", 8080));
-        System.out.println(socketChannel.finishConnect() + "--" + socketChannel.getRemoteAddress() + "--" + socketChannel.getLocalAddress());
+        System.out.println("连接情况：" + socketChannel.finishConnect()
+                + "，绑定远程端口：" + socketChannel.getRemoteAddress()
+                + "，绑定本地端口：" + socketChannel.getLocalAddress());
         Thread.sleep(10000);
         System.out.println("睡眠2s结束");
         ByteBuffer byteBuffer=ByteBuffer.allocate(64);
+
         byteBuffer.putInt(234567);
+        byteBuffer.putLong(1223L);
+
+        //设置很重要，否则数据无法写入
+        byteBuffer.flip();
         int count= socketChannel.write(byteBuffer);
-        System.out.println("向服务端发送数据"+count);
+
+        System.out.println("向服务端发送数据大小："+count+"字节");
     }
 }
--- nio/src/main/java/com/cliffyuan/nio/NioServer.java
+++ nio/src/main/java/com/cliffyuan/nio/NioServer.java
@@ -12,19 +12,24 @@ import java.nio.channels.SocketChannel;
 import java.util.Set;
 
 /**
+ * 绑定端口
+ * 注意：
+ * （1）设置serverSocketChannel的阻塞模式，只对serverSocketChannel.accept()方法起作用。
+ * （2）设置socketChannel.configureBlocking(false);只对socketChannel.read()和wirte（）方法起作用
  * Created by yuanyuanming on 14-7-10.
  */
 public class NioServer {
     public static void main(String[] args)throws IOException{
-        block();
-        //nonblock();
+        //acceptBlock();
+        //acceptNonblock();
+        acceptNonblockReadNonblock();
     }
 
     /**
      * 默认为阻塞模式
      * @throws IOException
      */
-    public static void block()throws IOException{
+    public static void acceptBlock()throws IOException{
         ServerSocketChannel serverSocketChannel=ServerSocketChannel.open();
         serverSocketChannel.bind(new InetSocketAddress(8080));
         SocketChannel socketChannel=null;
@@ -39,15 +44,17 @@ public class NioServer {
         }
         ByteBuffer byteBuffer= ByteBuffer.allocate(64);
         System.out.println("读取数据开始");
+        long start=System.currentTimeMillis();
         int count=socketChannel.read(byteBuffer);
-        System.out.println("读取数据结束，共多少字节:"+count+",数据为:"+byteBuffer.getInt());
+        byteBuffer.flip();
+        System.out.println("读取数据结束，共多少字节:"+count+",数据为:"+byteBuffer.getInt()+","+byteBuffer.getLong()+",耗时:"+(System.currentTimeMillis()-start));
     }
 
     /**
      * 非阻塞模式
      * @throws IOException
      */
-    public static void nonblock()throws IOException{
+    public static void acceptNonblock()throws IOException{
         ServerSocketChannel serverSocketChannel=ServerSocketChannel.open();
         serverSocketChannel.configureBlocking(false);
         serverSocketChannel.bind(new InetSocketAddress(8080));
@@ -64,8 +71,43 @@ public class NioServer {
 
         ByteBuffer byteBuffer= ByteBuffer.allocate(64);
         System.out.println("读取数据开始");
+        long start=System.currentTimeMillis();
         int count=socketChannel.read(byteBuffer);
-        System.out.println("读取数据结束，共多少字节:"+count+",数据为:"+byteBuffer.getInt());
+        byteBuffer.flip();
+        System.out.println("读取数据结束，共多少字节:"+count+",数据为:"+byteBuffer.getInt()+",耗时:"+(System.currentTimeMillis()-start));
+    }
+
+    /**
+     * 非阻塞模式
+     * @throws IOException
+     */
+    public static void acceptNonblockReadNonblock()throws IOException{
+        ServerSocketChannel serverSocketChannel=ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.bind(new InetSocketAddress(8080));
+        SocketChannel socketChannel=null;
+        while (true){
+            System.out.println("accept start");
+            socketChannel=serverSocketChannel.accept();
+            if(socketChannel!=null){
+                System.out.println("socketChannel="+socketChannel);
+                System.out.println("accept end,");
+                break;
+            }
+        }
+
+        socketChannel.configureBlocking(false);
+
+        ByteBuffer byteBuffer= ByteBuffer.allocate(64);
+        System.out.println("读取数据开始");
+        long start=System.currentTimeMillis();
+        int count=socketChannel.read(byteBuffer);
+        if(count>0){
+            byteBuffer.flip();
+            System.out.println("读取数据结束，共多少字节:"+count+",数据为:"+byteBuffer.getInt()+",耗时:"+(System.currentTimeMillis()-start));
+        }else {
+            System.out.println("读取数据结束，共多少字节:"+count+",无数据,耗时:"+(System.currentTimeMillis()-start));
+        }
     }
 
 }
