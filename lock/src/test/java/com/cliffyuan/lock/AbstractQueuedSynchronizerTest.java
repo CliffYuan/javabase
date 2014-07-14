f1de7573bad98472f4edcbefb792ebb9e4e9ca24
cliffyuan <xiaoniudu2012@gmail.com>
2014-07-14 23:15:12 +0800

    AQS

---
+++ lock/src/test/java/com/cliffyuan/lock/AbstractQueuedSynchronizerTest.java
@@ -0,0 +1,51 @@
+package com.cliffyuan.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by cliffyuan on 14-7-14.
+ */
+public class AbstractQueuedSynchronizerTest {
+
+    private ReentrantLock lock=new ReentrantLock();
+
+    public static void main(String[] args) {
+
+        AbstractQueuedSynchronizerTest test=new AbstractQueuedSynchronizerTest();
+        Thread thread=new Thread(new Work(test));
+        thread.start();
+
+        thread=new Thread(new Work(test));
+        thread.start();
+    }
+
+    public void testLock(){
+        try{
+            System.out.println("lock start");
+            lock.lock();
+
+            System.out.println("exe testLock");
+            Thread.sleep(1000000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+            System.out.println("unlock");
+        }
+    }
+
+}
+
+class Work implements Runnable{
+
+    AbstractQueuedSynchronizerTest test;
+
+    Work(AbstractQueuedSynchronizerTest test) {
+        this.test = test;
+    }
+
+    @Override
+    public void run() {
+        test.testLock();
+    }
+}
